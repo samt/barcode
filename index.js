56a18699c37c057a3cde5ada27ed6a54760e54e2
Sam Thompson <git@samt.us>
2014-05-28 23:01:40 -0700

    New API for barcodes

--- index.js
+++ index.js
@@ -2,79 +2,11 @@
  * barcode generator
  */
 
-var path = require('path');
-
-/*
- * Barcode fluent class
- */
-function BarcodeFluent(type) {
-	switch(type.toLowerCase()) {
-	//	case 'datamatrix':
-		case 'code39':
-	//	case 'code128':
-		case 'codabar':
-	//	case 'qrcode':
-	//	case 'upc':
-			break;
-		default:
-			throw new Error('Barcode type not supported');
-			return;
-	}
-
-	this.type = type;
-	this._width;
-	this._height;
-	this._data;
-
-	/*
-	 * set width
-	 * @param int w - width
-	 * @return BarcodeFluent
-	 */
-	this.width = function(w) {
-		this._width = w;
-		return this;
-	}
-
-	/*
-	 * set height
-	 * @param int h - height
-	 * @return BarcodeFluent
-	 */
-	this.height = function(h) {
-		this._height = h;
-		return this;
-	}
-
-	/*
-	 * set size (shortcut)
-	 * @param int h - height
-	 * @return BarcodeFluent
-	 */
-	this.size = function(w, h) {
-		this.width(w);
-		this.height(h);
-		return this;
-	}
-
-	this.data = function(d) {
-		this._data = d;
-		return this;
-	}
-
-	this.draw = function(callback) {
-		var lib = require('./lib/' + type);
-		lib.drawHTML(this._data, this._width, this._height, function(str) {
-			callback(str);
-		});
-	}
-}
-
-/*
- * helper
- */
-function barcode(type) {
-	return new BarcodeFluent(type);
-}
-
-module.exports = barcode;
+module.exports = {
+  Code39: require('./lib/code39.js');
+  Code128: {}; // wip
+  Codabar: require('./lib/codabar.js');
+  DataMatrix: {}; // wip
+  QRCode: {}; // wip
+  UPC: {}; // wip
+};
