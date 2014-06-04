04edae79b5a79b70baf1406b93937e9665b696b0
Sam Thompson <git@samt.us>
2014-06-03 20:32:58 -0700

    Revamped the API, should be final now
    Updates to the README will be committed soon

--- index.js
+++ index.js
@@ -2,11 +2,60 @@
  * barcode generator
  */
 
-module.exports = {
-  Code39: require('./lib/code39.js'),
-  Code128: {}, // wip
-  Codabar: require('./lib/codabar.js'),
-  DataMatrix: {}, // wip
-  QRCode: {}, // wip
-  UPC: {}, // wip
-};
+var fs = require('fs'),
+  path = require('path');
+
+module.exports = function (moduleName, options) {
+  return new Barcode(moduleName, options);
+}
+
+function Barcode(moduleName, options) {
+  this.barcode = require('./lib/' + moduleName.toLowerCase());
+  this.options = options;
+}
+
+Barcode.prototype.getStream = function (callback) {
+  this.barcode.createCode(this.options, function (err, stream) {
+    callback(err, stream);
+  });
+}
+
+Barcode.prototype.saveImage = function (outfile, callback) {
+  this.getStream(function (err, stream) {
+    if (err) return callback(err, '');
+
+    var ws = fs.createWriteStream(outfile);
+    stream.pipe(ws);
+
+    stream.on('end', function () {
+      callback(false, outfile);
+    });
+
+    stream.on('error', function (err) {
+      callback(err, '');
+    });
+  });
+}
+
+Barcode.prototype.getBase64 = function (callback) {
+  var type = this.options.type || 'PNG';
+
+  this.getStream(function (err, stream) {
+    if (err) return callback(err, '');
+
+    var imgBufs = [];
+    stream.on('data', function (chunk) {
+      imgBufs.push(chunk);
+    });
+
+    stream.on('end', function () {
+      var src = 'data:image/' + type + ';base64,';
+      src += Buffer.concat(imgBufs).toString('base64');
+      callback(false, src);
+    });
+
+    stream.on('error', function (err) {
+      callback(err, '')
+    });
+  });
+}
