3ebbcc584bed8008b2d67a83702288aff358fa4f
Sam Thompson <git@samt.us>
2014-08-05 11:02:13 -0800

    Replaced checksum calcuations within UPC files with the utils version

--- test/upc-a.js
+++ test/upc-a.js
@@ -5,15 +5,6 @@ describe('UPC-A', function () {
   describe('.properties', function () {
     it('should have exports', function () {
       assert.equal('function', typeof upc_a.createCode, 'createCode is missing');
-      assert.equal('function', typeof upc_a.getCheckDigit, 'getCheckDigit is missing');
-    });
-  });
-
-  describe('.getCheckDigit()', function () {
-    it('should calculate check digits correctly', function () {
-      assert.equal(9, upc_a.getCheckDigit('04878943035'));
-      assert.equal(0, upc_a.getCheckDigit('07017715424'));
-      assert.equal(2, upc_a.getCheckDigit('05543762275'));
     });
   });
 });
